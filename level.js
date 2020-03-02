f86ace7b213f14c5a33145ac231b74baa2b14f69
Chandrakanth Polisetty <chandrakanthpolisetty@gmail.com>
2020-03-02 11:51:47 -0600

    added

--- level.js
+++ level.js
@@ -1,5 +1,11 @@
-function convert(valNum) {
-  valNum = parseFloat(valNum);
-        document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+function convert(degree) {
+    var x;
+    if (degree == "C") {
+      x = document.getElementById("c").value * 9 / 5 + 32;
+      document.getElementById("f").value = Math.round(x);
+    } else {
+      x = (document.getElementById("f").value -32) * 5 / 9;
+      document.getElementById("c").value = Math.round(x);
     }
+  }
    
